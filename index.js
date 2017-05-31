87aa1f6321243ad52a1b7d64cb63d261a5083ee1
ivarmlee1 <ivarr5@gmail.com>
2017-05-30 23:14:24 -0700

    removed auto-merge comments

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ app.get('/', function (req, res) {
   res.render('index.html');
 });
 
-<<<<<<< HEAD
-=======
 console.log('Server running.');
 
->>>>>>> production
 app.listen(process.env.PORT || 3000);
